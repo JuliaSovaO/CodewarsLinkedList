6a34352bf2d58a7c74766d25e726c2065717863a
Sova Yuliia <sovaulia7@gmail.com>
2025-04-03 22:36:06 +0300

    Parse a linked list from a string final

--- Simple/Parse_list_from_string/parse.py
+++ Simple/Parse_list_from_string/parse.py
@@ -8,21 +8,23 @@ class Node:
         self.data = data
         self.next = next
 
-    # def __str__(self):
-    #     return f"Node({self.data}, {self.next})"
+    def __str__(self):
+        return f"Node({self.data}, {self.next})"
 
 
 def linked_list_from_string(s: str) -> Node:
     nodes = s.replace(" -> None", "").split(" -> ")
-    node = Node(nodes[0])
+    if nodes[0] == "None":
+        return
+    node = Node(int(nodes[0]))
     current = node
     for el in nodes[1:]:
-        current.next = Node(el)
+        current.next = Node(int(el)) if el != "None" else Node(el)
         current = current.next
     return node
 
 
 print(linked_list_from_string("1 -> 2 -> 3 -> None"))
-# Node(1, Node(2, Node(3)))
+# Node(1, Node(2, Node(3, None)))
 print(linked_list_from_string("0 -> 1 -> 4 -> 9 -> 16 -> None"))
-# Node(0, Node(1, Node(4, Node(9, Node(16)))))
+# Node(0, Node(1, Node(4, Node(9, Node(16, None)))))
